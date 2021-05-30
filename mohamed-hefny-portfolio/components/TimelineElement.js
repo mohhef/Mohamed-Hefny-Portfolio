4e17afec411cd163e560ef5f777a3faeeb05743e
mohhef <mohhef@rocketmail.com>
2021-05-30 18:38:59 -0400

    refactoring

--- mohamed-hefny-portfolio/components/TimelineElement.js
+++ mohamed-hefny-portfolio/components/TimelineElement.js
@@ -1,47 +1,38 @@
 import icon from "./icon"
 import { Row, Col } from 'reactstrap'
 import {
-    VerticalTimeline, VerticalTimelineElement
+    VerticalTimelineElement
 } from "react-vertical-timeline-component"
-import {timelineElementClick} from "@/utils/timeline"
+import { timelineElementClick } from "@/utils/timeline"
 
-const TimelineElement = ({ timeline, user, children }) => {
+const TimelineElement = ({ element, user, children }) => {
     const icons = icon;
+    const IconComp = icons[element?.type]?.iconName
+    const endDate = element.endDate ? element.endDate : "current"
+    const date = element.startDate + "-" + endDate
     return (
-        <div>
-            <VerticalTimeline >
-                {timeline.map((element, key) => {
-                    const IconComp = icons[element?.type]?.iconName
-                    const endDate = element.endDate ? element.endDate : "current"
-                    const date = element.startDate + "-" + endDate
-                    return (
-                        <VerticalTimelineElement
-                            onTimelineElementClick={user && timelineElementClick(user, element._id, element.website)}
-                            key={element._id}
-                            dateClassName="date"
-                            date={date}
-                            icon={IconComp ? <IconComp /> : null}
-                            iconStyle={icons[element?.type]?.style}
-                        >
-                            <Row>
-                                <Col xs="8" className="vertical-timeline-element-title">
-                                    {element.title}
-                                </Col>
-                                <Col xs="4" className="vertical-timeline-element-company">
-                                    {element.company}
-                                </Col>
-                            </Row>
-                            <h6 xs="6" className="vertical-timeline-element-location">
-                                {element.location}
-                            </h6>
-                            <p id="description">{element.description}</p>
-                            {children}
-                        </VerticalTimelineElement>
-                    )
-                })
-                }
-            </VerticalTimeline>
-        </div >
+        <VerticalTimelineElement
+            onTimelineElementClick={() => (window.location.href = `https://${element.website}`)}
+            key={element._id}
+            dateClassName="date"
+            date={date}
+            icon={IconComp ? <IconComp /> : null}
+            iconStyle={icons[element?.type]?.style}
+        >
+            <Row>
+                <Col xs="8" className="vertical-timeline-element-title">
+                    {element.title}
+                </Col>
+                <Col xs="4" className="vertical-timeline-element-company">
+                    {element.company}
+                </Col>
+            </Row>
+            <h6 xs="6" className="vertical-timeline-element-location">
+                {element.location}
+            </h6>
+            <p id="description">{element.description}</p>
+            {children}
+        </VerticalTimelineElement>
     )
 }
 
--- mohamed-hefny-portfolio/pages/timeline/index.js
+++ mohamed-hefny-portfolio/pages/timeline/index.js
@@ -2,25 +2,40 @@ import BaseLayout from "@/components/layouts/BaseLayout"
 import { useUser } from '@auth0/nextjs-auth0';
 import { Button } from 'reactstrap'
 import TimelineApi from '@/lib/api/timeline'
+import { isAuthorized } from '@/utils/auth0'
 import BasePage from "@/components/BasePage"
 import TimelineElement from "../../components/TimelineElement"
+import { VerticalTimeline } from "react-vertical-timeline-component"
+import { useRouter } from "next/router"
 
 
 const Timeline = ({ timeline }) => {
     const { user, isLoading } = useUser();
+    const router = useRouter();
     debugger
     return (
         <BaseLayout user={user} isLoading={isLoading}>
             <BasePage header="Timeline" className="timeline-page">
-                <TimelineElement timeline={timeline} user={user} >
-                    {
-                        user &&
-                        <>
-                            <Button className="mr-2" color="warning">Edit</Button>
-                            <Button color="danger">Delete</Button>
-                        </>
-                    }
-                </TimelineElement>
+                <VerticalTimeline >
+                    {timeline.map(element => {
+                        return (
+                            <TimelineElement element={element} user={user} >
+                                {
+                                    user && isAuthorized(user, "admin") &&
+                                    <>
+                                        <Button
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                router.push('/timeline/[id]/edit', `/timeline/${element._id}/edit`)
+                                            }}
+                                            className="mr-2" color="warning">Edit</Button>
+                                        <Button color="danger">Delete</Button>
+                                    </>
+                                }
+                            </TimelineElement>
+                        )
+                    })}
+                </VerticalTimeline >
             </BasePage>
         </BaseLayout >
     )
