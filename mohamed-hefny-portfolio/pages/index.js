c6fd772ce1194bca5a131828a4f430a633dc46aa
mohhef <mohhef@rocketmail.com>
2021-05-31 23:31:45 -0400

    add a flipping card

--- mohamed-hefny-portfolio/pages/index.js
+++ mohamed-hefny-portfolio/pages/index.js
@@ -1,71 +1,100 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import { Container, Row, Col } from 'reactstrap';
+import { useState, useRef, useEffect } from 'react'
 import { useUser } from '@auth0/nextjs-auth0';
 import Typed from 'react-typed';
 import React from 'react';
 
 const roles = ["Passionate Developer", "Enthusiastic learner", "Team Player", "Spring | React JS | Next JS", "MongoDb | Neo4j | Mysql", "Docker | Openshift | Kuburnetess", "Jenkins | Gitlab"]
-const Index = () =>{
+const Index = () => {
+  const [isFlipping, setIsFlipping] = useState(false);
   const { user, error, isLoading } = useUser();
-    return (
-      <BaseLayout 
-      navClass = "transparent"
-      user = {user}
-      isLoading = {isLoading}
-      className="cover">
-        <div className="main-section">
-          <div className="background-image">
-            <img src="/images/background-index.png" />
-          </div>
-          <Container>
-            <Row>
-              <Col md="6">
-                <div className="hero-section">
-                  <div className={`flipper`}>
-                    <div className="back">
-                      <div className="hero-section-content">
-                        <h2> Software Developer </h2>
-                        <div className="hero-section-content-intro">
-                          Have a look at my portfolio and job history.
+  const flipInterval = useRef();
+
+  useEffect(() => {
+    startAnimation();
+    //dispose function, dont call this startAnimation after we navigate away
+    return () => { flipInterval.current && clearInterval }
+  }, [])
+
+  const startAnimation = () => {
+    flipInterval.current = setInterval(() => {
+      //this callback has access to the current flipping value
+      setIsFlipping((prevFlipping) => !prevFlipping);
+    }, 5000)
+  }
+
+  return (
+    <BaseLayout
+      navClass="transparent"
+      user={user}
+      isLoading={isLoading}
+      className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}>
+      <div className="main-section">
+        <div className="background-image">
+          <img src="/images/background-index.png" />
+        </div>
+        <Container>
+          <Row>
+            <Col md="6">
+              <div className="hero-section">
+                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                  <div className="front">
+                    <div className="hero-section-content">
+                      <h2> Software Developer </h2>
+                      <div className="hero-section-content-intro">
+                        Have a look at my portfolio and job history.
                         </div>
-                      </div>
-                      <img className="image" src="/images/section-1.png" />
-                      <div className="shadow-custom">
-                        <div className="shadow-inner"> </div>
-                      </div>
+                    </div>
+                    <img className="image" src="/images/section-1.png" />
+                    <div className="shadow-custom">
+                      <div className="shadow-inner"> </div>
+                    </div>
+                  </div>
+                  <div className="back">
+                    <div className="hero-section-content">
+                      <h2> Software Developer </h2>
+                      <div className="hero-section-content-intro">
+                        Have a look at my portfolio and job history.
+                        </div>
+                    </div>
+                    <img className="image" src="/images/section-2.png" />
+                    <div className="shadow-custom shadow-custom-orange">
+                      <div className="shadow-inner"> </div>
                     </div>
                   </div>
                 </div>
-              </Col>
-              <Col md="6" className="hero-welcome-wrapper">
-                <div className="hero-welcome-text">
-                  <h1>
-                    Welcome To My Portfolio Website.
-                    Get informed, collaborate and discover projects I was working on throughout the years!
+              </div>
+            </Col>
+            <Col md="6" className="hero-welcome-wrapper">
+              <div className="hero-welcome-text">
+                <h1>
+                  Welcome To My Portfolio Website.
+                  Get informed, collaborate and discover projects I was working on throughout the years!
                   </h1>
-                </div>
-                <Typed
-                  loop
-                  typeSpeed={20}
-                  backSpeed={40}
-                  strings={roles}
-                  backDelay={1000}
-                  loopCount={0}
-                  showCursor
-                  className="self-typed"
-                  cursorChar="|"  
-                />
-                <div className="hero-welcome-bio">
-                  <h1>
-                    Let's take a look on my work.
+              </div>
+              <Typed
+                loop
+                typeSpeed={20}
+                backSpeed={40}
+                strings={roles}
+                backDelay={1000}
+                loopCount={0}
+                showCursor
+                className="self-typed"
+                cursorChar="|"
+              />
+              <div className="hero-welcome-bio">
+                <h1>
+                  Let's take a look on my work.
                   </h1>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </BaseLayout>
-    )
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </BaseLayout>
+  )
 }
 
 export default Index;
